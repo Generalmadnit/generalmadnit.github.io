5d53959b9e174b12906ca56995cf7c3bcd6e8dab
baramsivaramireddy <baramsivarami@gmail.com>
2022-04-11 22:20:00 +0530

    added fully functional nav

--- script.js
+++ script.js
@@ -14,25 +14,26 @@ function add_notes_nav( name,action){
 
   let notes_nav          =   document.querySelector('#nav-in-notes');
 
+
   // here 5 is department + >> + course + >> 
   if  (notes_nav.childElementCount >= 5  ){
     notes_nav.removeChild(notes_nav.lastChild)
     notes_nav.removeChild(notes_nav.lastChild)
     notes_nav.removeChild(notes_nav.lastChild)
-    notes_nav.removeChild(notes_nav.lastChild)
+    notes_nav.removeChild(notes_nav.lastChild)      
   }
-
-  let new_nav           =   document.createElement('span');
+ if  (document.querySelector('#note_title').innerHTML != 'Subjects')
+ {let new_nav           =   document.createElement('span');
   new_nav.innerHTML     =   name
   new_nav.setAttribute('onclick', action)
   
 
   let greaterthan           = document.createElement("span");
-  greaterthan.innerHTML  = ">>"
+  greaterthan.innerHTML  = "  >>  "
   notes_nav.appendChild(greaterthan)
   notes_nav.appendChild(new_nav);
 
-
+ }
 }
 
 
@@ -105,10 +106,12 @@ async function get_subjects(event){
     document.title  = event.target.innerHTML
     const response =  await fetch('https://kitsbrigade.herokuapp.com/api/departments/'.concat(event.target.innerHTML))
     const data = await response.json();
-    add_notes_nav(event.target.innerHTML,'get_subjects(event)')
+    
     console.log(data)
     removeClassToElement(main_area,'loader')
+    add_notes_nav(event.target.innerHTML,'get_subjects(event)')
     change_notes_title("Subjects")
+    
     ga('send', 'pageview', document.title);
       for (let i = 0; i < data.length; i++) {
           let new_div =  document.createElement("div");
@@ -132,9 +135,10 @@ async function get_subjects(event){
 
       // empty string is passed to  below function . Because last (subject nav should not call again)
 
-      add_notes_nav(event.target.innerHTML,'')
+      
       removeClassToElement(main_area,'loader')
       change_notes_title("units")
+      add_notes_nav(event.target.innerHTML,'')
       ga('send', 'pageview', document.title);
   
       for (let i = 0; i < data.length; i++) {
